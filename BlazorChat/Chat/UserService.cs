5e335676a9ab9d4f3a53a6e6bbe150abe5562a2e
gracjan.winiarski <gracjan.winiarski@SF-07>
2023-03-14 18:11:31 +0100

    Dodaj pliki projektów.

---
+++ BlazorChat/Chat/UserService.cs
@@ -0,0 +1,32 @@
+﻿namespace BlazorChat.Chat
+{
+    public class UserService
+    {
+        private readonly Dictionary<string, string> _users = new();
+        public void Add(string connectionId,string username) {
+            _users[connectionId] = username;
+        }
+
+        public void RemoveByName(String username) {
+        _users.Remove(username);
+        }
+
+        public string GetConnectionIdByName(string username) {
+            if (_users.ContainsKey(username))
+            {
+                return _users[username];
+            }
+
+            return null;
+        }
+
+        public IEnumerable<(string connectionId,string username)> GetAll() {
+            foreach (var kvp in _users)
+            {
+                yield return (kvp.Value, kvp.Key);
+            }
+        }
+
+
+    }
+}
